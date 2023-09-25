81d478a317c1e53a38103ee9cf64f7c0dd536d04
Darren Ng <Un1gfn@gmail.com>
2023-09-25 19:45:26 +0800

    todo: continue with unit_12 or switch to 20230606.tex?

--- mutate.py
+++ mutate.py
@@ -1,5 +1,7 @@
 #!/opt/homebrew/bin/python3 -u
-#!/opt/homebrew/bin/python3
+
+# [%] verbatim line
+# [\] force syllable break
 
 # 받침 발음 변화
 # https://en.wikipedia.org/wiki/Korean_language_and_computers#Hangul_syllables_block
@@ -7,6 +9,19 @@
 
 import sys
 import string
+
+# BOOK_1_UNIT_00_PAGE_44
+BATCHIM_K = (1, 24, 2)
+BATCHIM_T = (7, 25, 19, 20, 22, 23, 27)
+BATCHIM_P = (17, 26)
+BATCHIM_KX = BATCHIM_K + (3,)
+BATCHIM_TX = BATCHIM_T
+BATCHIM_PX = BATCHIM_K + (18,)
+FH2F = {
+     6: 4, # ㄶ ㄴ
+    15: 8, # ㅀ ㄹ
+    27: 0, # ㅎ
+}
 
 class Jamos:
     pass
@@ -23,41 +38,85 @@ def h2j(h):
 def j2h(j):
     return chr(j.i * 588 + j.m * 28 + j.f + 44032)
 
-def mutate1(h):
+def ruleset_mono(h):
     j = h2j(h)
-    if j.m == 7 and j.i in [ 0, 17, 18, ]: # b01u02p089
+    # BOOK_1_UNIT_02_PAGE_089
+    if j.i in (0, 17, 18) and j.m == 7:
         j.m = 5
     return j2h(j)
 
-def mutate2(h1, h2):
-    # b01u03p109
-    l = [ # (  F,  I, ),
-        (  1,  0, ), # ㄱ
-        (  2,  1, ), # ㄲ
-        (  4,  2, ), # ㄴ
-        (  7,  3, ), # ㄷ
-        (  8,  5, ), # ㄹ
+# /Users/darren/tex/20230605.dfi48n/20230606.tex
+def ruleset_duo(hx, hy):
 
-        ( 16,  6, ), # ㅁ
-        ( 17,  7, ), # ㅂ
+    jx, jy = h2j(hx), h2j(hy)
 
-        ( 19,  9, ), # ㅅ
-        ( 20, 10, ), # ㅆ
-        ( 21, 11, ), # ㅇ
-        ( 22, 12, ), # ㅈ
+    # BOOK_1_UNIT_05_PAGE_150 # execute before BOOK_1_UNIT_03_PAGE_109
+    if (jx.f, jy.i, jy.m, jy.f) in [(17, 11, 17, 0), (17, 11, 17, 1)]:
+        jx.f, jy.i = 16, 2
+    if jx.f in BATCHIM_P and jy.i in (0, 9):
+        jy.i += 1
 
-        ( 23, 14, ), # ㅊ
-        ( 24, 15, ), # ㅋ
-        ( 25, 16, ), # ㅌ
-        ( 26, 17, ), # ㅍ
-        ( 27, 18, ), # ㅎ
-    ]
-    j1, j2 = h2j(h1), h2j(h2)
-    if j2.i == 11:
-        for (f,i) in l:
-            if f == j1.f:
-                j1.f, j2.i = 0, i
-    return j2h(j1), j2h(j2)
+    # BOOK_1_UNIT_11_PAGE_284 # execute before BOOK_1_UNIT_06_PAGE_172
+    # BOOK_1_UNIT_11_PAGE_284 # execute before BOOK_1_UNIT_10_PAGE_263
+    i2ih = {
+         0: 15, # ㅋ
+         3: 16, # ㅌ
+        12: 14, # ㅊ
+    }
+    if jx.f in FH2F.keys() and jy.i in i2ih.keys():
+        jx.f, jy.i = FH2F[jx.f], i2ih[jy.i]
+    if jy.i == 18:
+        if jx.f in BATCHIM_KX: jx.f, jy.i = 0, 15 # ㅋ
+        if jx.f in BATCHIM_TX: jx.f, jy.i = 0, 16 # ㅌ
+        if jx.f in BATCHIM_PX: jx.f, jy.i = 0, 17 # ㅍ
+
+    # BOOK_1_UNIT_06_PAGE_172 # execute before BOOK_1_UNIT_03_PAGE_109
+    if jx.f in FH2F.keys() and jy.i == 11:
+        jx.f = FH2F[jx.f]
+
+    # BOOK_1_UNIT_03_PAGE_109 # execute before BOOK_1_UNIT_10_PAGE_263
+    f2i = { # F0: (F, I)
+         1: ( 0,  0), # ㄱ
+         2: ( 0,  1), # ㄲ
+         3: ( 1,  9), # ㄳ ㄱ ㅅ
+         4: ( 0,  2), # ㄴ
+         5: ( 4, 12), # ㄵ ㄴ ㅈ
+        # 6 # ㄶ
+         7: ( 0,  3), # ㄷ
+         8: ( 0,  5), # ㄹ
+         9: ( 8,  0), # ㄺ ㄹ ㄱ
+        10: ( 8,  6), # ㄻ ㄹ ㅁ
+        11: ( 8,  7), # ㄼ ㄹ ㅂ
+        12: ( 8,  9), # ㄽ ㄹ ㅅ
+        13: ( 8, 16), # ㄾ ㄹ ㅌ
+        14: ( 8, 17), # ㄿ ㄹ ㅍ
+        # 15 # ㅀ
+        16: ( 0,  6), # ㅁ
+        17: ( 0,  7), # ㅂ
+        18: (17,  9), # ㅄ ㅂ ㅅ
+        19: ( 0,  9), # ㅅ
+        20: ( 0, 10), # ㅆ
+        # 21 # ㅇ
+        22: ( 0, 12), # ㅈ
+        23: ( 0, 14), # ㅊ
+        24: ( 0, 15), # ㅋ
+        25: ( 0, 16), # ㅌ
+        26: ( 0, 17), # ㅍ
+        # 27 # ㅎ
+    }
+    if jx.f in f2i.keys() and jy.i == 11:
+        jx.f, jy.i = f2i[jx.f]
+
+    # BOOK_1_UNIT_04_PAGE_129
+    # BOOK_1_UNIT_10_PAGE_263
+    if jx.f in BATCHIM_K+BATCHIM_T+BATCHIM_P and jy.i in (0, 3, 7, 9, 12):
+        jy.i += 1
+
+    # BOOK_1_UNIT_07_PAGE_193
+    if jx.f in BATCHIM_P and jy.i in (2, 6):
+        jx.f = 16
+
+    return j2h(jx), j2h(jy)
 
 def emit(h, hh):
     if h == hh:
@@ -73,13 +132,14 @@ def mutate(filename):
     uPrev = ""
     uPrevM = ""
     sNonHangul = ""
-    Nonpronounceable = string.punctuation.replace("%","") + ' ' + '、，。・”’'
+    Nonpronounceable = str1 = ''.join(x for x in string.punctuation if x not in ("%", "\\")) + ' ' + '、，。・”’'
+    file = open(filename, "r")
 
     # N = 0
     # def v():
     #     print([uPrev, uPrevM, sNonHangul, uCur])
 
-    file = open(filename, "r")
+
     while True:
 
         uCur = file.read(1)
@@ -97,7 +157,7 @@ def mutate(filename):
             break
         elif 0*588+0*28+0+44032 <= ord(uCur) <= 18*588+20*28+27+44032: # CaseHangul
             if uPrev:
-                uPrevMM, uCurM = mutate2(uPrevM, mutate1(uCur))
+                uPrevMM, uCurM = ruleset_duo(uPrevM, ruleset_mono(uCur))
                 r += emit(uPrev, uPrevMM)
                 r += sNonHangul
                 sNonHangul ,uPrev, uPrevM = "", uCur, uCurM
@@ -105,11 +165,12 @@ def mutate(filename):
                 r += sNonHangul
                 sNonHangul, uPrev, uPrevM = "", uCur, uCur
         else: # CaseNonhangul
-            if uCur in Nonpronounceable: # CaseNonhangulNonpronounceableNocut
+            if uCur in Nonpronounceable: # NonpronounceableNocut
                 sNonHangul += uCur
                 pass
-            else: # CaseNonhangulPronounceableCut
-                sNonHangul += uCur
+            else: # PronounceableCut
+                if uCur != "\\":
+                    sNonHangul += uCur
                 # G1WIX9CT calc.l
                 # G1WIX9CT mutate.py
                 if uCur == "%":
